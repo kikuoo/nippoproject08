0fc0be76603dfb4245c472a661e3ec1a179c4c5a
kikuoo <kikuoo505@gmail.com>
2024-10-11 20:02:00 +0900

    コミット05

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\kikuo\myproject\myproject\nippoproject08\venv
+set VIRTUAL_ENV=C:\webproject\nippo09\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
