1441a891f2d5f2d9d70ee83d968b0ce332259919
kikuoo <kikuoo505@gmail.com>
2024-10-13 20:25:49 +0900

    コミット07

--- src/nippo/views.py
+++ src/nippo/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from random import randint
+
+def NippoListView(request):
+    return render(request, "nippo/nippo-list.html")
+    
+    
+def nippoDetailView(request):
+    template_name="nippo/nippo-detail.html"
+    random_int = randint(1,10)
+    ctx = {
+        "random_number": random_int,
+    }
+    return render(request, template_name, ctx)
 
 # Create your views here.
